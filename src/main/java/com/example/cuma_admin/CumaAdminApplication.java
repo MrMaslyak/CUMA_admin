d7ae8c4e3ffa390c3e4f1464c238c1fcdb3f481b
Maslyak <129599445+MrMaslyak@users.noreply.github.com>
2026-01-17 22:37:55 +0200

    up infrastructure

---
+++ src/main/java/com/example/cuma_admin/CumaAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.cuma_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CumaAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cuma_admin/CumaAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cuma_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumaAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
